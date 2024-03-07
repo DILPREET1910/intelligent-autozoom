78b8609c08337ed8c00f83b4870fbf22c4ff1ad7
DILPREET1910 <dsahney1910@gmail.com>
2024-03-07 10:02:37 +0530

    shutter buttons

--- lib/widgets/shutter.dart
+++ lib/widgets/shutter.dart
@@ -13,6 +13,41 @@ class _ShutterWidgetState extends State<ShutterWidget> {
     return Container(
       height: MediaQuery.of(context).size.height * 0.1,
       color: Colors.black.withOpacity(0.3),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.camera),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Container(
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white,
+              ),
+              padding: const EdgeInsets.all(15),
+              child: const Icon(Icons.camera_alt),
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.black.withOpacity(0.1),
+              ),
+              padding: const EdgeInsets.all(12),
+              child: const Icon(
+                Icons.autorenew_sharp,
+                color: Colors.white,
+                size: 30,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
